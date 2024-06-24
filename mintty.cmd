b9dfa13551aa6e7d12c17b956f3b4764cc6a97e4
pavelrazlivaev <pasha.razlivaev@yandex.ru>
2024-06-24 15:55:24 +0300

    fix problem msys-2.0.dll

--- mintty.cmd
+++ mintty.cmd
@@ -1,3 +1,3 @@
 @echo off
-app\mintty --nodaemon -o AppID=GitForWindows.Bash -o AppLaunchCmd="app\git-bash.exe" -o --store-taskbar-properties --app\bash --login -i
+app\mintty --nodaemon -o AppID=GitForWindows.Bash -o AppLaunchCmd="app\git-bash.exe" -o --store-taskbar-properties -- app\bash --login -i
 exit 0
